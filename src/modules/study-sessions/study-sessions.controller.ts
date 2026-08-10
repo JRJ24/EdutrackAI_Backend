8468e29dec638dc99733e0ad4d1ccf4bb01a1934
Yeliana Diaz <117601413+yeliana267@users.noreply.github.com>
2026-08-09 22:37:41 -0400

    feat: recalculate plan after study sessions

--- src/modules/study-sessions/study-sessions.controller.ts
+++ src/modules/study-sessions/study-sessions.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { getErrorResponse } from "../../helpers/http-error";
+import { triggerAdaptiveRecalculation } from "../adaptive-engine/adaptive-engine.events";
 import { studySessionsService } from "./study-sessions.service";
 
 const getAll = async (req: Request, res: Response) => {
@@ -85,6 +86,7 @@ const getBySubject = async (req: Request, res: Response) => {
 const create = async (req: Request, res: Response) => {
   try {
     const data = await studySessionsService.create(req.body);
+    triggerAdaptiveRecalculation(data.user.id, "study_session_saved");
 
     return res.status(201).json({
       ok: true,
@@ -104,6 +106,7 @@ const create = async (req: Request, res: Response) => {
 const update = async (req: Request, res: Response) => {
   try {
     const data = await studySessionsService.update(String(req.params.id), req.body);
+    triggerAdaptiveRecalculation(data.user.id, "study_session_saved");
 
     return res.status(200).json({
       ok: true,
